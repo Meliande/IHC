e34caf04bc9c737e0801476d8932ec6b78739fc8
Rodrigo Meliande <rodrigomeliande@msn.com>
2021-10-18 11:43:00 -0300

    A

--- function.js
+++ function.js
@@ -4,7 +4,6 @@ var $nav = $('nav');
 $header_top.find('a').on('click', function() {
   $(this).parent().toggleClass('open-menu');
 });
-
 $('#fullpage').fullpage({
   sectionsColor: ['#3dcfa1', '#348899', '#ff8b20', '#ff5757', '#ffd03c', '#3dcfa1', '#348899', '#ff8b20', '#ff5757', '#ffd03c'],
   sectionSelector: '.vertical-scrolling',
@@ -13,15 +12,10 @@ $('#fullpage').fullpage({
   controlArrows: false,
   anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection', 'seventhSection', 'eighthSection', 'ninethSection', 'tenthSection'],
   menu: '#menu',
-
   afterLoad: function(anchorLink, index) {
     $header_top.css('background', 'rgba(0, 47, 77, .3)');
     $nav.css('background', 'rgba(0, 47, 77, .25)');
-    if (index == 5) {
-        $('#fp-nav').hide();
-      }
   },
-
   onLeave: function(index, nextIndex, direction) {
     if(index == 5) {
       $('#fp-nav').show();
