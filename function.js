f2edb58a70504965de8d380790d3a73b33720ea0
Rodrigo Meliande <rodrigomeliande@msn.com>
2021-10-19 15:42:12 -0300

    a

--- function.js
+++ function.js
@@ -5,7 +5,7 @@ $header_top.find('a').on('click', function() {
   $(this).parent().toggleClass('open-menu');
 });
 $('#fullpage').fullpage({
-  sectionsColor: ['#3dcfa1', '#348899', '#ff8b20', '#ff5757', '#ffd03c', '#ff8b20', '#3dcfa1', '#348899', '#ff8b20', '#ff5757', '#ffd03c'],
+  sectionsColor: ['#3dcfa1', '#348899', '#ff8b20', '#ff5757', '#0288d1', '#ff8b20', '#3dcfa1', '#348899', '#ff8b20', '#ff5757', '#ffd03c'],
   sectionSelector: '.vertical-scrolling',
   navigation: true,
   slidesNavigation: true,
